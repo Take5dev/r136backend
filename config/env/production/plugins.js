cf2ad1636a39537b89b26b9a4a0e4ce12f8cf9ce
Maksim <info@take5agency.com>
2023-09-27 17:27:56 +0400

    Supabase Plugin

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,51 +1,50 @@
 module.exports = ({ env }) => ({
-    //
-    email: {
-        config: {
-            provider: 'nodemailer',
-            providerOptions: {
-                host: env('SMTP_HOST', 'smtp.example.com'),
-                port: env('SMTP_PORT', 587),
-                auth: {
-                    user: env('SMTP_USERNAME'),
-                    pass: env('SMTP_PASSWORD'),
-                },
-                secure: true
-                // ... any custom nodemailer options
-            },
-            settings: {
-                defaultFrom: 'php.del.by@gmail.com',
-                defaultReplyTo: 'php.del.by@gmail.com',
-            },
+  //
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
+        secure: true,
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "php.del.by@gmail.com",
+        defaultReplyTo: "php.del.by@gmail.com",
+      },
     },
-    upload: {
-        config: {
-            provider: "strapi-provider-upload-supabase-v4",
-            providerOptions: {
-                apiUrl: env("SUPABASE_API_URL"),
-                apiKey: env("SUPABASE_API_KEY"),
-                bucket: env("SUPABASE_BUCKET"),
-                directory: env("SUPABASE_DIRECTORY"),
-                options: {},
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
-        },
+  },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase-strage",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        bucketPrefix: "",
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
     },
-    graphql: {
-        config: {
-            endpoint: '/graphql',
-            shadowCRUD: true,
-            playgroundAlways: false,
-            depthLimit: 10,
-            amountLimit: 100,
-            apolloServer: {
-                tracing: false,
-            },
-        },
+  },
+  graphql: {
+    config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
+      playgroundAlways: false,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+      },
     },
+  },
 });
