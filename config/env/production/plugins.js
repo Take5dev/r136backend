179ead3d36dff00befcaaebd9d3c7b702365b6d1
Maksim <info@take5agency.com>
2023-09-27 17:33:30 +0400

    Supabase Plugin 2

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -23,7 +23,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-upload-supabase-strage",
       providerOptions: {
-        apiUrl: env("SUPABASE_API_URL"),
+        url: env("SUPABASE_API_URL"),
         apiKey: env("SUPABASE_API_KEY"),
         bucket: env("SUPABASE_BUCKET"),
         bucketPrefix: "",
