cbe63b5bc6523ed22ea81cea4df09d992875964d
Maksim <info@take5agency.com>
2023-09-27 16:39:40 +0400

    Providers

---
+++ config/env/production/middleware.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => [
+    "strapi::errors",
+    //"strapi::security",
+    {
+        name: "strapi::security",
+        config: {
+            contentSecurityPolicy: {
+                directives: {
+                    "default-src": ["'self'"],
+                    "img-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL"), env('RENDER_EXTERNAL_URL', '')],
+                },
+            },
+        },
+    },
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+];
