5d8745fd4ace3b7f9513b96ff70d00c1a9891f43
Maksim <info@take5agency.com>
2023-09-27 18:32:57 +0400

    Email for request

--- src/api/request/content-types/request/lifecycles.js
+++ src/api/request/content-types/request/lifecycles.js
@@ -5,7 +5,7 @@ module.exports = {
       to: "max.digital@yandex.ru",
       from: "php.del.by@gmail.com",
       subject: "New Request from the website",
-      text: `Hello Denis,<br />there is a new request from the website<br /><br />Name: <b>${result.name}</b><br />Title: <b>${result.title}</b><br />Company: <b>${result.company}</b><br />Country: <b>${result.country}</b><br />Email: <b>${result.email}</b><br />Cover letter: <b>${result.message}</b>`,
+      text: `Hello Denis,<br />there is a new request from the website<br /><br />Name: <b>${result.Name}</b><br />Title: <b>${result.Title}</b><br />Company: <b>${result.Company}</b><br />Country: <b>${result.Country}</b><br />Email: <b>${result.Email}</b><br />Cover letter: <b>${result.message}</b>`,
     });
   },
 };
