fe4d94c67ac8ff3158727bbd569be544240a8d60
Maksim <info@take5agency.com>
2023-09-28 13:42:50 +0400

    Lifecycles to ENV

--- src/api/request/content-types/request/lifecycles.js
+++ src/api/request/content-types/request/lifecycles.js
@@ -1,11 +1,11 @@
-module.exports = {
+module.exports = ({ env }) => ({
   afterCreate(event) {
     const { result } = event;
     strapi.plugins["email"].services.email.send({
-      to: "max.digital@yandex.ru",
-      from: "php.del.by@gmail.com",
+      to: env("ADMIN_EMAIL"),
+      from: env("SMTP_FROM"),
       subject: "New Request from the website",
       text: `Hello Denis,<br />there is a new request from the website<br /><br />Name: <b>${result.Name}</b><br />Title: <b>${result.Title}</b><br />Company: <b>${result.Company}</b><br />Country: <b>${result.Country}</b><br />Email: <b>${result.Email}</b><br />Cover letter: <b>${result.message}</b>`,
     });
   },
-};
+});
